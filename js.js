9ee2e269efab490cea42a875ad037a82eff3c6e6
Pedro <pedrofagiane4@gmail.com>
2024-05-01 10:55:28 -0300

    concerto

--- js.js
+++ js.js
@@ -2,15 +2,15 @@ const form = document.getElementById('Valores')
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
-    const ValorAinp = document.getElementById('nx');
-    const ValorBinp = document.getElementById('ny');
+    const ValorAinp = document.getElementById('nx').value;
+    const ValorBinp = document.getElementById('ny').value;
 
     const ValorA = parseFloat(ValorAinp);
     const ValorB = parseFloat(ValorBinp);
 
     const falso = `Tente novamente`;
-    const verdade = `Esta correto o número ${campoB} é maior que o número ${campoA}`;
-    if(!isNaN(ValorA) &&  !isan(ValorB)) {
+    const verdade = `Esta correto o número ${ValorB} é maior que o número ${ValorA}`;
+    if(!isNaN(ValorA) &&  !isNaN(ValorB)) {
         if(ValorA >= ValorB){
             const containerFalso = document.querySelector('.Diferentes');
             containerFalso.innerHTML = falso;
